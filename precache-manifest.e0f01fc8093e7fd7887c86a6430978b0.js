eda4000797f0f4189826fbc3aa3bb120ceedfcdd
Andriy Ivanyk <andriy@MacBook-Air.home>
2021-06-23 04:31:54 +0100

    Updates

---
+++ precache-manifest.e0f01fc8093e7fd7887c86a6430978b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4b25b25afd92d1275567db8bd9fe66",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f2f8645514ada537648",
+    "url": "./static/css/main.3d57ec2b.chunk.css"
+  },
+  {
+    "revision": "9f6387954c8ee55a67c1",
+    "url": "./static/js/2.27768066.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.27768066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2f8645514ada537648",
+    "url": "./static/js/main.5f29d040.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
